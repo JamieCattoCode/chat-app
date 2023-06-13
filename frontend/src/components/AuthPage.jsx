31f641a3b7b8f7ad281851fdc4b6bc353bd7b5e1
JamieCattoCode <jamietcatto@gmail.com>
2023-06-13 08:22:47 +0200

    Basic form for auth

---
+++ frontend/src/components/AuthPage.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+function AuthPage({ onAuth }) {
+  const [username, setUsername] = useState('');
+
+  const handleNameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onAuth({ username, secret: username });
+  };
+
+  return (
+    <div className="background">
+      <form onSubmit={handleSubmit} className="form-card">
+        <div className="form-title">Welcome!</div>
+        <div className="form-subtitle">Set a username to get started.</div>
+        <div className="auth">
+          <div className="auth-label">Username</div>
+          <input
+            className="auth-input"
+            name="username"
+            value={username}
+            onChange={handleNameChange}
+          />
+          <button className="auth-button" type="submit">
+            Enter
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default AuthPage;
