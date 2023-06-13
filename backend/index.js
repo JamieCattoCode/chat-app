df1e97e726be43ed1eaeb6fb6592aa827329bdbc
JamieCattoCode <jamietcatto@gmail.com>
2023-06-13 08:01:41 +0200

    Initial commit

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios')
+require('dotenv').config({path: __dirname+'/../.env'});
+
+const app = express();
+
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+const privateKey = process.env.chatEnginePrivateKey
+
+app.post('/authenticate', async (req, res) => {
+    const { username } = req.body;
+    try {
+        const chatEngineResponse = await axios.put(
+            'https://api.chatengine.io/users/',
+            {username, secret: username, first_name: username},
+            {headers: {"PRIVATE-KEY": privateKey}}
+        );
+        return res.status(chatEngineResponse.status).json(chatEngineResponse.data);
+    } catch (error) {
+        return res.status(error.response.status).json(error.response.data);
+    }
+});
+
+app.listen(3001);
